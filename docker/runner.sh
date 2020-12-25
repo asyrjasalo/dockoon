424c0ee83da4c0ad49274592fb024b17ee4be807
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2020-12-25 19:00:39 +0200

    Exit gracefully on SIGINT and SIGTERM

--- docker/runner.sh
+++ docker/runner.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+trap "echo -e '\nCaught ^C, exiting gracefully...' ;
+node_modules/.bin/mockoon stop all ; exit 0" SIGINT SIGTERM
+
 node_modules/.bin/mockoon "$@"
 
 [ "$1" = "start" ] && tail -f ~/.mockoon-cli/logs/*.log | while read -r
