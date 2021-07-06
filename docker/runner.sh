e6990019c0086b751e71d36b5191c0e91eea2357
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2021-07-06 23:04:55 +0300

    Trap with success on --version, --help etc.

--- docker/runner.sh
+++ docker/runner.sh
@@ -4,6 +4,9 @@ trap "echo -e '\nCaught ^C, exiting gracefully...' ;
 node_modules/.bin/mockoon-cli stop all ; exit 0" INT TERM
 # or use https://docs.docker.com/engine/reference/run/#specify-an-init-process
 
+# exit successfully on --version, --help etc.
+trap "exit 0" EXIT
+
 node_modules/.bin/mockoon-cli "$@"
 
 [ "$1" = "start" ] && tail -f ~/.mockoon-cli/logs/*.log | while read -r
